592a0785bc5a957f63476ed8e596e1759f2cabdf
poiuytrewq0987654321 <bundushathurh@gmail.com>
2022-02-16 20:03:21 +0100

    update baza.sql

--- baza.sql
+++ baza.sql
@@ -2,10 +2,10 @@
 CREATE DATABASE yacht_for_rent;
 CREATE TABLE yacht_for_rent.reservation (
 id INT(3) UNSIGNED AUTO_INCREMENT,
-model VARCHAR(50) NOT NULL,
-lastname VARCHAR(50) NOT NULL,
+model VARCHAR(20) NOT NULL,
+lastname VARCHAR(100) NOT NULL,
 telephone int(9) NOT NULL,
-email VARCHAR(50) NOT NULL,
+email VARCHAR(60) NOT NULL,
 date_from date NOT NULL,
 date_to date NOT NULL,
 PRIMARY KEY (id)
